eb851c2720862e3cdfeefe8af9b239a3b3d3003d
Nikodem Wójcik <01nikodemw@gmail.com>
2024-06-13 16:45:26 +0200

    feat: add validation for post, put and delete

--- todo-backend/src/tasks/create-task.dto.ts
+++ todo-backend/src/tasks/create-task.dto.ts
@@ -1,12 +1,26 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsIn, IsNotEmpty, Length } from 'class-validator';
+import { TASK_STATUS, TASK_STATUS_ARRAY } from './task-status.constants';
 
 export class CreateTaskDto {
   @ApiProperty({ example: 'Title of the task' })
+  @IsNotEmpty({ message: 'Title is required' })
+  @Length(1, 255, {
+    message: 'Title must be a string between 1 and 255 characters',
+  })
   title: string;
 
   @ApiProperty({ example: 'Description of the task' })
+  @IsNotEmpty({ message: 'Description is required' })
+  @Length(1, 4000, {
+    message: 'Description must be between 1 and 4000 characters',
+  })
   description: string;
 
-  @ApiProperty({ example: 'todo' })
+  @ApiProperty({ example: TASK_STATUS.TODO })
+  @IsNotEmpty({ message: 'Status is required' })
+  @IsIn(TASK_STATUS_ARRAY, {
+    message: `Status must be one of the following: ${TASK_STATUS_ARRAY.join(', ')}`,
+  })
   status: string;
 }
--- todo-backend/src/tasks/tasks.service.ts
+++ todo-backend/src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Task } from './task.entity';
@@ -23,10 +23,18 @@ export class TasksService {
   }
 
   async update(id: number, task: Partial<Task>): Promise<void> {
+    const existingTask = await this.tasksRepository.findOneBy({ id });
+    if (!existingTask) {
+      throw new NotFoundException(`Task with ID ${id} not found`);
+    }
     await this.tasksRepository.update(id, task);
   }
 
   async remove(id: number): Promise<void> {
+    const existingTask = await this.tasksRepository.findOneBy({ id });
+    if (!existingTask) {
+      throw new NotFoundException(`Task with ID ${id} not found`);
+    }
     await this.tasksRepository.delete(id);
   }
 }
