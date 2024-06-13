72af9687cdbe2498fbe0e127b2cb4e2e931a5b79
Nikodem Wójcik <01nikodemw@gmail.com>
2024-06-13 17:10:07 +0200

    feat: add mobile initial app

--- todo-backend/src/main.ts
+++ todo-backend/src/main.ts
@@ -6,6 +6,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://example.com', 'http://another-example.com'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization, X-Requested-With',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
