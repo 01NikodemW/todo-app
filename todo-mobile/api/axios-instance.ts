98825156434d3b20b8364958dc1e5999d9df080d
Nikodem Wójcik <01nikodemw@gmail.com>
2024-06-14 16:28:53 +0200

    feat: add readme

--- todo-mobile/api/axios-instance.ts
+++ todo-mobile/api/axios-instance.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig } from "axios";
 
 const config: AxiosRequestConfig = {
-  baseURL: "http://192.168.0.101:3000",
+  baseURL: process.env.EXPO_PUBLIC_API_URL,
 };
 export const axiosInstance = axios.create(config);
